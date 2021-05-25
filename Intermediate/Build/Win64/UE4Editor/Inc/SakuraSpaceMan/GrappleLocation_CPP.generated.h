19d70fcd98cd21bb2991cccbc12777a075d949fb
MemeMerc <68625209+MemeMerc@users.noreply.github.com>
2021-05-25 17:43:18 +1200

    Grapple Aim
    It will always select the grapple point closest to center of the screen.

--- Intermediate/Build/Win64/UE4Editor/Inc/SakuraSpaceMan/GrappleLocation_CPP.generated.h
+++ Intermediate/Build/Win64/UE4Editor/Inc/SakuraSpaceMan/GrappleLocation_CPP.generated.h
@@ -59,7 +59,9 @@ DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AGrappleLocation_CPP); \
 
 
 #define Test_Source_SakuraSpaceMan_Public_GrappleLocation_CPP_h_13_PRIVATE_PROPERTY_OFFSET \
-	FORCEINLINE static uint32 __PPO__SphereGrappleLocation() { return STRUCT_OFFSET(AGrappleLocation_CPP, SphereGrappleLocation); }
+	FORCEINLINE static uint32 __PPO__SphereGrappleLocation() { return STRUCT_OFFSET(AGrappleLocation_CPP, SphereGrappleLocation); } \
+	FORCEINLINE static uint32 __PPO__vScreenLocation() { return STRUCT_OFFSET(AGrappleLocation_CPP, vScreenLocation); } \
+	FORCEINLINE static uint32 __PPO__fScreenLength() { return STRUCT_OFFSET(AGrappleLocation_CPP, fScreenLength); }
 
 
 #define Test_Source_SakuraSpaceMan_Public_GrappleLocation_CPP_h_10_PROLOG
