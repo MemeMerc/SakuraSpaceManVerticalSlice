878b8439dcc7b107f42b15a58cde3e1162331be0
AnonDcrest <dylanchan2000@gmail.com>
2021-06-10 14:21:46 +1200

    Audio pack added

--- Intermediate/Build/Win64/UE4Editor/Inc/SakuraSpaceMan/SakuraSpaceMan.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/SakuraSpaceMan/SakuraSpaceMan.init.gen.cpp
@@ -21,7 +21,7 @@ void EmptyLinkFunctionForGeneratedCodeSakuraSpaceMan_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x79E1AE0A,
+				0xF9F53C8D,
 				0x3F12AA82,
 				METADATA_PARAMS(nullptr, 0)
 			};
