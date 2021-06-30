594a05eb590170ff6e99bed10643d337a1831b1c
MemeMerc <68625209+MemeMerc@users.noreply.github.com>
2021-06-30 23:27:55 +1200

    Audio

--- Intermediate/Build/Win64/UE4Editor/Inc/SakuraSpaceMan/SakuraSpaceMan.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/SakuraSpaceMan/SakuraSpaceMan.init.gen.cpp
@@ -21,7 +21,7 @@ void EmptyLinkFunctionForGeneratedCodeSakuraSpaceMan_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x423148C9,
+				0xCC9C5DF1,
 				0xFD4D8AE4,
 				METADATA_PARAMS(nullptr, 0)
 			};
