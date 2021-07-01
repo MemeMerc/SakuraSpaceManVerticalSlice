86a260623dbfb3e011572afbdb01d57773aa7f24
AnonDcrest <dylanchan2000@gmail.com>
2021-07-01 13:44:44 +1200

    eee

--- Intermediate/Build/Win64/UE4Editor/Inc/SakuraSpaceMan/ScoreBoardWid_CPP.generated.h
+++ Intermediate/Build/Win64/UE4Editor/Inc/SakuraSpaceMan/ScoreBoardWid_CPP.generated.h
@@ -13,10 +13,10 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 #endif
 #define SAKURASPACEMAN_ScoreBoardWid_CPP_generated_h
 
-#define SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_SPARSE_DATA
-#define SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_RPC_WRAPPERS
-#define SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_RPC_WRAPPERS_NO_PURE_DECLS
-#define SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_INCLASS_NO_PURE_DECLS \
+#define SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_SPARSE_DATA
+#define SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_RPC_WRAPPERS
+#define SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesUScoreBoardWid_CPP(); \
 	friend struct Z_Construct_UClass_UScoreBoardWid_CPP_Statics; \
@@ -25,7 +25,7 @@ public: \
 	DECLARE_SERIALIZER(UScoreBoardWid_CPP)
 
 
-#define SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_INCLASS \
+#define SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_INCLASS \
 private: \
 	static void StaticRegisterNativesUScoreBoardWid_CPP(); \
 	friend struct Z_Construct_UClass_UScoreBoardWid_CPP_Statics; \
@@ -34,7 +34,7 @@ public: \
 	DECLARE_SERIALIZER(UScoreBoardWid_CPP)
 
 
-#define SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_STANDARD_CONSTRUCTORS \
+#define SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_STANDARD_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API UScoreBoardWid_CPP(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UScoreBoardWid_CPP) \
@@ -47,7 +47,7 @@ private: \
 public:
 
 
-#define SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_ENHANCED_CONSTRUCTORS \
+#define SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_ENHANCED_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API UScoreBoardWid_CPP(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
 private: \
@@ -60,34 +60,34 @@ DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UScoreBoardWid_CPP); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UScoreBoardWid_CPP)
 
 
-#define SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_PRIVATE_PROPERTY_OFFSET \
+#define SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_PRIVATE_PROPERTY_OFFSET \
 	FORCEINLINE static uint32 __PPO__PlayersScore_Txt() { return STRUCT_OFFSET(UScoreBoardWid_CPP, PlayersScore_Txt); } \
 	FORCEINLINE static uint32 __PPO__Timer_Txt() { return STRUCT_OFFSET(UScoreBoardWid_CPP, Timer_Txt); } \
 	FORCEINLINE static uint32 __PPO__BestScore_Txt() { return STRUCT_OFFSET(UScoreBoardWid_CPP, BestScore_Txt); } \
 	FORCEINLINE static uint32 __PPO__BestTime_Txt() { return STRUCT_OFFSET(UScoreBoardWid_CPP, BestTime_Txt); }
 
 
-#define SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_12_PROLOG
-#define SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_GENERATED_BODY_LEGACY \
+#define SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_12_PROLOG
+#define SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_GENERATED_BODY_LEGACY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_PRIVATE_PROPERTY_OFFSET \
-	SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_SPARSE_DATA \
-	SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_RPC_WRAPPERS \
-	SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_INCLASS \
-	SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_STANDARD_CONSTRUCTORS \
+	SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_PRIVATE_PROPERTY_OFFSET \
+	SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_SPARSE_DATA \
+	SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_RPC_WRAPPERS \
+	SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_INCLASS \
+	SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_STANDARD_CONSTRUCTORS \
 public: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
-#define SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_GENERATED_BODY \
+#define SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_PRIVATE_PROPERTY_OFFSET \
-	SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_SPARSE_DATA \
-	SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
-	SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_INCLASS_NO_PURE_DECLS \
-	SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_ENHANCED_CONSTRUCTORS \
+	SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_PRIVATE_PROPERTY_OFFSET \
+	SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_SPARSE_DATA \
+	SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_INCLASS_NO_PURE_DECLS \
+	SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h_15_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -95,7 +95,7 @@ PRAGMA_ENABLE_DEPRECATION_WARNINGS
 template<> SAKURASPACEMAN_API UClass* StaticClass<class UScoreBoardWid_CPP>();
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID SakuraSpaceManVerticalSlice_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h
+#define CURRENT_FILE_ID SSMVS_Source_SakuraSpaceMan_Public_ScoreBoardWid_CPP_h
 
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
